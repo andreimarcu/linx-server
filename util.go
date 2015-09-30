cf39a98cddcec5799e6e89651b07c25eb8406164
andreimarcu <andrei@marcu.net>
2015-09-29 22:12:02 -0400

    Formatting

--- util.go
+++ util.go
@@ -8,6 +8,31 @@ import (
 // from http://stackoverflow.com/a/31832326
 var src = rand.NewSource(time.Now().UnixNano())
 
+const letterBytes = "abcdefghijklmnopqrstuvwxyz1234567890"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func randomString(n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
 var mimeToExtension = map[string]string{
 	"application/andrew-inset":                                                  "ez",
 	"application/applixware":                                                    "aw",
@@ -776,29 +801,4 @@ var mimeToExtension = map[string]string{
 	"video/x-sgi-movie":                                                         "movie",
 	"video/x-smv":                                                               "smv",
 	"x-conference/x-cooltalk":                                                   "ice",
-}
-
-const letterBytes = "abcdefghijklmnopqrstuvwxyz1234567890"
-const (
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
-
-func randomString(n int) string {
-	b := make([]byte, n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
-	}
-
-	return string(b)
 }
