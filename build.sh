e86f19ecb1d674c964aa7af3f99f65f40529431d
andreimarcu <andrei@marcu.net>
2017-10-10 22:14:53 -0700

    Add linx-cleanup to build script

--- build.sh
+++ build.sh
@@ -64,3 +64,31 @@ GOOS=windows GOARCH=amd64 go build -o "$name"windows-amd64.exe
 GOOS=windows GOARCH=386 go build -o "$name"windows-386.exe
 
 cd ..
+
+
+cd linx-cleanup
+name="../binairies/""$version""/linx-cleanup-v""$version""_"
+
+GOOS=darwin GOARCH=amd64 go build -o "$name"osx-amd64
+
+GOOS=darwin GOARCH=386 go build -o "$name"osx-386
+
+GOOS=freebsd GOARCH=amd64 go build -o "$name"freebsd-amd64
+
+GOOS=freebsd GOARCH=386 go build -o "$name"freebsd-386
+
+GOOS=openbsd GOARCH=amd64 go build -o "$name"openbsd-amd64
+
+GOOS=openbsd GOARCH=386 go build -o "$name"openbsd-386
+
+GOOS=linux GOARCH=arm go build -o "$name"linux-arm
+
+GOOS=linux GOARCH=amd64 go build -o "$name"linux-amd64
+
+GOOS=linux GOARCH=386 go build -o "$name"linux-386
+
+GOOS=windows GOARCH=amd64 go build -o "$name"windows-amd64.exe
+
+GOOS=windows GOARCH=386 go build -o "$name"windows-386.exe
+
+cd ..
